cd9044165058e36a0a98582361ec73164ece0532
Vinicios Gomes <viniciusgomesviana@gmail.com>
2020-05-10 23:16:20 -0300

    Challenger finalized

---
+++ src/database/migrations/1589149527580-AddFieldCategorieIdtoTransaction.ts
@@ -0,0 +1,36 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddFieldCategorieIdtoTransaction1589149527580
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        name: 'TransactionCategoriy',
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategoriy');
+    await queryRunner.dropColumn('transactions', 'category_id');
+  }
+}
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    // Existe? Delete | error
+
+    const transactionRepositoy = getCustomRepository(TransactionRepository);
+
+    const transaction = await transactionRepositoy.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist.', 400);
+    }
+
+    await transactionRepositoy.remove(transaction);
   }
 }
 
